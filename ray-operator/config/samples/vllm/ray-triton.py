9b285c8b40414394325738070d38164346b56d53
Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>
2025-02-12 15:18:11 -0600

    Update ray-triton.py
    Signed-off-by: Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>

--- ray-operator/config/samples/vllm/ray-triton.py
+++ ray-operator/config/samples/vllm/ray-triton.py
@@ -22,21 +22,24 @@ class TritonDeployment:
             log_info=False,
         )
         self._triton_server.start(wait_until_ready=True)
+        self._triton_server.load("llama3-8b-instruct")
+        if not self._llama3_8b.ready():
+                    raise Exception("Model not ready")
 
     @app.get("/generate")
     def generate(self, prompt: str) -> None:
-        if not self._triton_server.model("llama3-8b-instruct").ready():
-            try:
-                self._llama3_8b = self._triton_server.load("llama3-8b-instruct")
-                if not self._llama3_8b.ready():
-                    raise Exception("Model not ready")
-            except Exception as error:
-                print(f"Error can't load llama3_8b model, {error}")
-                return
-        responses = []
-        for response in self._llama3_8b.infer(inputs={"prompt": [[prompt]]}):
-            responses.append(response)
+        #if not self._triton_server.model("llama3-8b-instruct").ready():
+        #    try:
+        #        self._llama3_8b = self._triton_server.load("llama3-8b-instruct")
+        #        if not self._llama3_8b.ready():
+        #            raise Exception("Model not ready")
+        #    except Exception as error:
+        #        print(f"Error can't load llama3_8b model, {error}")
+        #        return
+        #responses = []
+        #for response in self._llama3_8b.infer(inputs={"prompt": [[prompt]]}):
+        #    responses.append(response)
 
-        return responses
+        return "test success"
 
 triton = TritonDeployment.bind()
