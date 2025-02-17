40afc88aa448666d31d9ff26367f747fbc114630
Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>
2025-02-17 14:02:19 -0600

    Update ray-triton.py
    Signed-off-by: Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>

--- ray-operator/config/samples/vllm/ray-triton.py
+++ ray-operator/config/samples/vllm/ray-triton.py
@@ -71,8 +71,7 @@ class TritonDeployment:
         for response in responses:
           output.append(response.outputs["text_output"].to_bytes_array())
 
-        generated_text = b"".join(output)
-        print(generated_text)
+        print(b"".join(output))
 
         return "success"
 
