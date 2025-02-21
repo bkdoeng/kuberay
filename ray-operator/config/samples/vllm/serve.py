06c5b609c1852fb70d465ef720facb5378d918e0
Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>
2025-02-21 15:14:51 -0600

    Update serve.py
    Signed-off-by: Boopathy Kannappan <86243143+bkdoeng@users.noreply.github.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -135,4 +135,12 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 
 
 model = build_app(
-    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
+    {"model": os.environ['MODEL_ID'], 
+     "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], 
+     "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'],
+     "disable-log-requests": True,
+     "distributed-executor-backend": "mp",
+     "gpu-memory-utilization": 0.65,
+     "enable-chunked-prefill": False,
+     "enforce-eager": True,
+    })
